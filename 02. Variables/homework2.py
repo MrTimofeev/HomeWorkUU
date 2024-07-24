29b1915b96eb0e9896bb95302f4e3580c1bf953c
MrTimofeev <89146685+MrTimofeev@users.noreply.github.com>
2024-07-24 16:04:10 +0300

    Update homework2.py

--- 02. Variables/homework2.py
+++ 02. Variables/homework2.py
@@ -4,7 +4,7 @@
 count_complete_DZ = 12
 count_of_house_spent = 1.5
 name_course = 'Python'
-time_to_task = count_complete_DZ/count_of_house_spent
+time_to_task = count_of_house_spent/count_complete_DZ
 
 # Первый способ вывода
 print('Курс:', name_course,', всего задач:', count_complete_DZ, ', затрачено часов:', count_of_house_spent, ', среднее время выполнения ', time_to_task, 'часа.')
