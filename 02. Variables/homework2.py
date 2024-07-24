c438390d34e35f23ac1e39bdfd25da83af1cc71e
MrTimofeev <89146685+MrTimofeev@users.noreply.github.com>
2024-07-24 15:38:28 +0300

    Third completed task
    Оne more step on a long journey!!)

---
+++ 02. Variables/homework2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#coding: utf-8
+
+count_complete_DZ = 12
+count_of_house_spent = 1.5
+name_course = 'Python'
+time_to_task = count_complete_DZ/count_of_house_spent
+
+# Первый способ вывода
+print('Курс:', name_course,', всего задач:', count_complete_DZ, ', затрачено часов:', count_of_house_spent, ', среднее время выполнения ', time_to_task, 'часа.')
+# Второй способ вывода
+print(f'Курс: {name_course}, всего задач:{count_complete_DZ}, затрачено часов: {count_of_house_spent}, среднее время выполнения {time_to_task} часа.')
+
