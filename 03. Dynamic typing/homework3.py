afb658868eb0f3d3f21f0d4ec6c8f21a7f48fc2b
MrTimofeev <89146685+MrTimofeev@users.noreply.github.com>
2024-07-24 16:35:19 +0300

    Fourth completed task
    Оne more step on a long journey!!)

---
+++ 03. Dynamic typing/homework3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#coding: utf-8
+
+name = "Tim"
+print("Вывод имени:", name)
+age = 19
+print("Вывод возраста:", age)
+age = age + 6
+print("Вывод нового возраста:", age)
+is_student = True
+print("Is Student:", is_student)
+
+
