8aa453a20e2d3c6803f3794c8f813df8db16151a
MrTimofeev <89146685+MrTimofeev@users.noreply.github.com>
2024-07-29 14:40:22 +0300

    Twenty-first completed task
    Оne more step on a long journey!!)

---
+++ 20. Modules and packages/fake_math.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# coding: utf-8
+
+
+def divide(first, second):
+    if second == 0:
+        return "Ошибка"
+    else:
+        return first / second
---
+++ 20. Modules and packages/true_math.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# coding: utf-8
+from math import inf
+
+def divide(first, second):
+    if second == 0:
+        return inf
+    else:
+        return first / second
