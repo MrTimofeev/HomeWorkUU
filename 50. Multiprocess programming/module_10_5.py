8e752972b02a44714facab0d0f1fc14cccfe25c4
MrTimofeev <89146685+MrTimofeev@users.noreply.github.com>
2024-09-17 14:37:59 +0300

    Update module_10_5.py

--- 50. Multiprocess programming/module_10_5.py
+++ 50. Multiprocess programming/module_10_5.py
@@ -6,24 +6,28 @@ import datetime
 
 def read_info(name):
     all_data = []
-    for i in name:
-        print(i)
-        with open(file=i, mode="r", encoding="utf-8") as file:
-            for i in file:
-                all_data.append(file.readline())
+    print(name)
+    with open(file=name, mode="r", encoding="utf-8") as file:
+        for line in file:
+            # Проверяем, является ли строка пустой
+            if line == '':
+                break
+
+            all_data.append(line)
 
 
 filenames = [
     f'.\\50. Multiprocess programming\\file {number}.txt' for number in range(1, 5)]
 
 start = datetime.datetime.now()
-read_info(filenames)
+for filename in filenames:
+    read_info(filename)
 end = datetime.datetime.now()
 print(end-start)
 
 # if __name__ == '__main__':
 #     start = datetime.datetime.now()
 #     with multiprocessing.Pool(processes=4) as pool:
-#         pool.map(read_info, (filenames,))
+#         pool.map(read_info, filenames)
 #     end = datetime.datetime.now()
 #     print(end-start)
