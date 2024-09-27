626c5a44cd0114952aab5f43b9be81be25be6028
MrTimofeev <89146685+MrTimofeev@users.noreply.github.com>
2024-09-27 17:03:16 +0300

    Sixty completed tasks
    Оne more step on a long journey!!)

---
+++ 59. Message Handlers/module_13_2.py
@@ -0,0 +1,36 @@
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+import asyncio
+from os import getenv
+import dotenv
+
+# Загрузка виртуального окружения
+dotenv.load_dotenv()
+# получение токена
+API_TOKEN = getenv("API_TOKEN")
+
+
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message):
+    print("Привет я бот помогающий твоему здоровью!")
+
+
+@dp.message()
+async def all_message(message: Message):
+    print("Введите команду /start, чтобы начать общение.")
+
+
+async def main() -> None:
+    bot = Bot(token=API_TOKEN, default=DefaultBotProperties(
+        parse_mode=ParseMode.HTML))
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
