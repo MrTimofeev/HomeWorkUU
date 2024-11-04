14ab26ed362c2725ab345c2461fcce04c78aee0f
MrTimofeev <89146685+MrTimofeev@users.noreply.github.com>
2024-11-04 16:04:27 +0300

    Update module_16_4.py

--- 72. Pydantic Data Models/module_16_4.py
+++ 72. Pydantic Data Models/module_16_4.py
@@ -11,13 +11,26 @@ class User(BaseModel):
     username: str
     age: int
 
+
 @app.get("/users")
 async def get_users() -> list:
     return users
 
+
 @app.post("/user/{username}/{age}")
-async def add_user(user: User) -> User:
-    user.id = len(users) +1
+async def add_user(username: str = Path(min_length=5, max_length=20, description="Enter username", example="UrbanUser"), age: int = Path(ge=18, le=120, description="Enter age", example="24")) -> User:
+    if len(users) == 0:
+        user = User(
+            id=1,
+            username=username,
+            age=age
+        )
+    else:
+        user = User(
+            id=max(user.id for user in users)+1,
+            username=username,
+            age=age
+        )
     users.append(user)
     return user
 
@@ -30,16 +43,13 @@ async def update_user(user_id: int, username: str = Path(min_length=5, max_lengt
             i.age = age
             return i
     raise HTTPException(status_code=404, detail="User was not found")
-            
-            
-            
+
 
 @app.delete("/user/{user_id}")
-async def delete_user(user_id:int) -> User:
+async def delete_user(user_id: int) -> User:
     for i in users:
         if i.id == user_id:
             users.remove(i)
             return i
-        
+
     raise HTTPException(status_code=404, detail="User was not found")
-    
